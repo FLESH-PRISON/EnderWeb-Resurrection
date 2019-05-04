2d1aac2d83c2d27a9da508b34afd609a2840ef2c
khuxkm fbexl <khuxkm@ttm.sh>
2019-05-04 01:05:56 -0400

    Fixed main page render

--- server/testserver.py
+++ server/testserver.py
@@ -3,8 +3,11 @@ from flask import Flask
 MAIN_PAGE = """<?xml version="1.0" charset="UTF-8" ?>
 <page>
 <body>
-<drawing align="center">bbb.............d+b.b.eee.444.bb..d+bbb.e.e..44.b.b.d+b...ee..4.4.b...d+b...eee.444.b...d</drawing>
+<div align="center" color="white" fill="true"></div>
+<drawing align="center"><image>bbb.............d+b.b.eee.444.bb..d+bbb.e.e..44.b.b.d+b...ee..4.4.b...d+b...eee.444.b...d</image></drawing>
+<div align="center" color="white" fill="true"></div>
 <div align="center" color="gray">Find out more at</div>
+<div align="center" color="white" fill="true"></div>
 <div align="center" color="black">https://github.com/MineRobber9000/enderweb-phoenix</div>
 </body>
 </page>
@@ -15,3 +18,5 @@ app = Flask(__name__)
 @app.route("/getPage/000000")
 def index():
 	return MAIN_PAGE
+
+app.run(port=80)
